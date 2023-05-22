3b820302a5c41aea33aad96b5721aec3cf83143b
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-05-22 14:21:14 -0500

    Minor changes to cartpole

--- cartpole_simulator.py
+++ cartpole_simulator.py
@@ -14,36 +14,43 @@ def cartpole_state_fn(obs):
 
 def expert_policy(state: LDIPSSample):
     if state.get("theta") > 0:
-        return 1
+        return "1"
     else:
-        return 0
+        return "0"
 
 
-def simulate(policy, render=False):
+def simulate(policy, max_steps=10000, render=False):
     env = gym.make("CartPole-v1", render_mode="human" if render else None)
     env.reset()
     trace = []
     action = env.action_space.sample()
+    steps = 0
+    sat = False
     while True:
         obs, reward, done, truncated, info = env.step(action)
         prev_action = action
         state = LDIPSSample(obs, cartpole_state_fn, prev_action, None)
-        action = policy(state)
-        sample = LDIPSSample(obs, cartpole_state_fn, int(prev_action), int(action))
+        action = int(policy(state))
+        sample = LDIPSSample(obs, cartpole_state_fn, str(int(prev_action)), str(int(action)))
         trace.append(sample.sample)
+        steps += 1
         if done:
             break
+        if steps > max_steps:
+            sat = True
+            break
     env.close()
-    return trace
+    return sat, trace
 
 
 if __name__ == "__main__":
     ## Simulate the expert policy and collect initial samples.
     traces = []
     for i in range(5):
-        traces.extend(simulate(expert_policy))
+        _, trace = simulate(expert_policy)
+        traces.append(trace)
 
     with open("cartpole_samples.json", "w") as f:
-        json.dump(traces, f, indent=4)
+        json.dump([s for t in traces for s in t], f, indent=4)
 
     ## Run LDIPS on the samples to get policy1.
